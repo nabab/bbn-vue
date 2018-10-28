c388d8bc485d206daac8c80e8cbcec9c8062581a
nabab <thomas.nabet@gmail.com>
2018-10-28 12:50:45 +0100

    Added error screens in tabnav

--- src/components/tabnav/tabnav.js
+++ src/components/tabnav/tabnav.js
@@ -566,6 +566,95 @@
         }
       },
 
+      blackhole(ele){
+        // Global Animation Setting
+        window.requestAnimFrame =
+          window.requestAnimationFrame ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame ||
+          window.oRequestAnimationFrame ||
+          window.msRequestAnimationFrame ||
+          function(callback) {
+            window.setTimeout(callback, 1000/60);
+          };
+
+// Global Canvas Setting
+        var canvas = ele;
+        var ctx = canvas.getContext('2d');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+
+// Particles Around the Parent
+        function Particle(x, y, distance) {
+          this.angle = Math.random() * 2 * Math.PI;
+          this.radius = Math.random() ;
+          this.opacity =  (Math.random()*5 + 2)/10;
+          this.distance = (1/this.opacity)*distance;
+          this.speed = this.distance*0.00003;
+
+          this.position = {
+            x: x + this.distance * Math.cos(this.angle),
+            y: y + this.distance * Math.sin(this.angle)
+          };
+
+          this.draw = function() {
+            ctx.fillStyle = "rgba(255,255,255," + this.opacity + ")";
+            ctx.beginPath();
+            ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, false);
+            ctx.fill();
+            ctx.closePath();
+          }
+          this.update = function() {
+            this.angle += this.speed;
+            this.position = {
+              x: x + this.distance * Math.cos(this.angle),
+              y: y + this.distance * Math.sin(this.angle)
+            };
+            this.draw();
+          }
+        }
+
+        function Emitter(x, y) {
+          this.position = { x: x, y: y};
+          this.radius = 30;
+          this.count = 3000;
+          this.particles = [];
+
+          for(var i=0; i< this.count; i ++ ){
+            this.particles.push(new Particle(this.position.x, this.position.y, this.radius));
+          }
+        }
+
+
+        Emitter.prototype = {
+          draw: function() {
+            ctx.fillStyle = "rgba(0,0,0,1)";
+            ctx.beginPath();
+            ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, false);
+            ctx.fill();
+            ctx.closePath();
+          },
+          update: function() {
+            for(var i=0; i< this.count; i++) {
+              this.particles[i].update();
+            }
+            this.draw();
+          }
+        }
+
+
+        var emitter = new Emitter(canvas.width/2, canvas.height/2);
+
+        function loop() {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          emitter.update();
+          requestAnimFrame(loop);
+        }
+
+        loop();
+      },
+
       add(obj_orig, idx){
         const vm = this;
         let obj = $.extend({}, obj_orig),
@@ -662,6 +751,7 @@
           //bbn.fn.log("ACTIVATE1", url);
           this.activate(url);
         }
+        vm.tabs[idx].state = 'loading';
         return bbn.fn.post(finalURL, {_bbn_baseURL: vm.fullBaseURL}, (d) => {
           if ( d.content ){
             if ( d.url ){
@@ -711,6 +801,21 @@
               vm.activate(d.current);
             });
           }
+        }, (xhr, textStatus, errorThrown) => {
+          bbn.fn.log(arguments);
+          if ( this.isValidIndex(idx) ){
+            this.tabs[idx].state = xhr.status;
+            this.tabs[idx].loading = false;
+            this.tabs[idx].loaded = true;
+            this.tabs[idx].menu = false;
+            this.tabs[idx].title = bbn._('Error') + ' ' + xhr.status;
+            if ( this.tabs[idx].load !== false ){
+              this.tabs[idx].load = null;
+            }
+            this.navigate(url);
+            this.activate(url);
+          }
+          bbn.fn.log(arguments)
         })
       },
 
@@ -1255,6 +1360,26 @@
       'bbns-loader': {
         name: 'bbns-loader',
         props: ['source']
+      },
+      'bbns-error-404': {
+        props: {
+          selected: {
+            type: Boolean
+          }
+        },
+        mounted(){
+          this.$parent.blackhole(this.getRef('particle'));
+        }
+      },
+      'bbns-error-500': {
+        props: {
+          selected: {
+            type: Boolean
+          }
+        },
+        mounted(){
+          this.$parent.blackhole(this.getRef('particle'));
+        }
       },
       'bbns-tab': {
         name: 'bbns-tab',
