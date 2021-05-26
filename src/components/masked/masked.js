c02b13d3cf4bdc1093ce060b21f7e5f95ed25edb
Thomas Nabet <thomas.nabet@gmail.com>
2021-05-26 16:06:27 +0200

    Removed the prop 'pattern' from inputComponent and added it to bbn-masked.

--- src/components/masked/masked.js
+++ src/components/masked/masked.js
@@ -36,6 +36,13 @@
       promptChar: {
         type: String,
         default: '_'
+      },
+      /**
+         * Defines the pattern of this elemenet
+         * @prop {String} pattern
+         */
+       pattern: {
+        type: String
       }
     },
     data(){
