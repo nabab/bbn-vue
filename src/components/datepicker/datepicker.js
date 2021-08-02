dca188abb0ad4f51030a7cd58381aeb6c1073708
Thomas Nabet <thomas.nabet@gmail.com>
2021-08-02 13:10:59 +0200

    Fixed setDate method on datepicker and datetimepicker components

--- src/components/datepicker/datepicker.js
+++ src/components/datepicker/datepicker.js
@@ -244,7 +244,7 @@
        * @fires setValue
        */
       setDate(val){
-        this.setValue(dayjs(val, 'YYYY-MM-DD').isValid() ? dayjs(val, 'YYYY-MM-DD').format(this.getValueFormat(val)) : '');
+        this.setValue(dayjs(val, this.getValueFormat(val)).isValid() ? dayjs(val, this.getValueFormat(val)).format(this.getValueFormat(val)) : '');
       },
       /**
        * Sets the value.
