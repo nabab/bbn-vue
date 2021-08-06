94be7cc2b18a81e9edfed8fadf8a9382fcab77dc
Thomas Nabet <thomas.nabet@gmail.com>
2021-08-06 12:02:10 +0200

    Fixed datetimepicker

--- src/components/datetimepicker/datetimepicker.js
+++ src/components/datetimepicker/datetimepicker.js
@@ -229,15 +229,15 @@
        * @fires setValue
       */
       setDate(val){
-        val = dayjs(val, this.getValueFormat(val)).isValid() ? dayjs(val, this.getValueFormat(val)) : '';
+        val = dayjs(val, 'YYYY-MM-DD').isValid() ? dayjs(val, 'YYYY-MM-DD') : '';
         if ( this.value && val ){
           let mom = dayjs(this.value.toString(), this.getValueFormat(this.value.toString()));
-          val.hour(mom.hour()).minute(mom.minute());
+          val = dayjs(dayjs(val, 'YYYY-MM-DD').hour(mom.hour())).minute(mom.minute());
           if ( this.showSecond ){
-            val.second(mom.second());
+            val = dayjs(val).second(mom.second());
           }
         }
-        this.setValue(val ? val.format(this.getValueFormat(val)) : '');
+        this.setValue(val ? dayjs(val).format(this.getValueFormat(dayjs(val).format(this.currentValueFormat))) : '');
       },
       /**
        * Sets the value format from 'HH:mm' to 'HH:mm:ss'.
@@ -251,9 +251,9 @@
         val = dayjs(val, 'HH:mm' + (this.showSecond ? ':ss' : ''));
         if ( this.value ){
           let mom = dayjs(this.value.toString(), this.getValueFormat(this.value.toString()));
-          val.date(mom.date()).month(mom.month()).year(mom.year());
+          val = dayjs(dayjs(dayjs(val).date(mom.date())).month(mom.month())).year(mom.year());
         }
-        this.setValue(val.format(this.getValueFormat(val)));
+        this.setValue(dayjs(val).format(this.getValueFormat(dayjs(val).format(this.currentValueFormat))));
       },
       /**
        * Sets the value.
