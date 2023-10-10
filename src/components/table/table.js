c08c62d4d679ca072f047cd1483c0a65ce627b10
Mirko Argentino <mirko@bbn.solutions>
2023-10-10 16:46:00 +0200

    Bugfix on bbn-table

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1013,6 +1013,7 @@
                 expander: true,
                 expanded: isExpanded
               };
+              expanderIndex = tmp.index;
               if (this.selection
                 && (!bbn.fn.isFunction(this.selection)
                   || this.selection(tmp))
@@ -2493,6 +2494,21 @@
       toggleExpanded(idx) {
         if (this.currentData[idx]) {
           if (this.allExpanded) {
+            bbn.fn.each(this.items, item => {
+              if (!this.groupable) {
+                if (!this.currentExpanded.includes(item.index)) {
+                  this.currentExpanded.push(item.index);
+                }
+              }
+              else if ((this.group !== false)
+                && this.cols[this.group]
+                && this.cols[this.group].field
+                && (item.data[this.cols[this.group].field] !== undefined)
+                && !this.currentExpandedValues.includes(item.data[this.cols[this.group].field])
+              ) {
+                this.currentExpandedValues.push(item.data[this.cols[this.group].field]);
+              }
+            });
             this.allExpanded = false;
           }
           if (
